7bf1cdf5fac04e303428233df82c87732711eebb
Aroldo Hernandez Armas <aroldohernandezarmas@gmail.com>
2020-06-10 23:51:41 +0200

    Add logger

---
+++ src/main/java/com/lasmakinas/Application.java
@@ -0,0 +1,13 @@
+package com.lasmakinas;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world");
+    }
+}
