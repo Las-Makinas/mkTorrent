11a109fa0f746f545db4ba3815293a2167d755a1
Aroldo Hernandez Armas <aroldohernandezarmas@gmail.com>
2020-06-26 18:54:21 +0200

    Make change for compatibility with java 8

--- src/main/java/com/lasmakinas/configuration/datasource/FileDataSource.java
+++ src/main/java/com/lasmakinas/configuration/datasource/FileDataSource.java
@@ -1,5 +1,7 @@
 package com.lasmakinas.configuration.datasource;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -16,12 +18,13 @@ public class FileDataSource implements DataSource {
 
     @Override
     public String getData() {
-        try (FileReader fileReader = new FileReader(filename, charset)) {
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader(new File(filename, String.valueOf(charset))))) {
             final int BUFFER_SIZE = 1024;
             char[] buffer = new char[BUFFER_SIZE];
             StringBuilder sb = new StringBuilder();
             int size;
-            while ((size = fileReader.read(buffer, 0, BUFFER_SIZE)) != -1) {
+            while ((size = bufferedReader.read(buffer, 0, BUFFER_SIZE)) != -1) {
                 sb.append(buffer, 0, size);
             }
             return sb.toString();
--- src/main/java/com/lasmakinas/configuration/token/BeEncodeToken.java
+++ src/main/java/com/lasmakinas/configuration/token/BeEncodeToken.java
@@ -1,15 +1,20 @@
 package com.lasmakinas.configuration.token;
 
-
 public class BeEncodeToken extends NonLeafToken {
 
     public BeEncodeToken(String input) {
         super(input);
-        Token token = switch (input.charAt(0)) {
-            case 'd' -> new DictionaryToken(input);
-            case 'l' -> new ListToken(input);
-            default -> new PrimitiveToken(input);
-        };
+        Token token = null;
+        switch (input.charAt(0)) {
+            case 'd':
+                token = new DictionaryToken(input);
+                break;
+            case 'l':
+                token = new ListToken(input);
+                break;
+            default:
+                token = new PrimitiveToken(input);
+        }
         this.length = token.length;
         children.add(token);
 
